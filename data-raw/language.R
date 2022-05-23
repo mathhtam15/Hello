a818413e2ce6eb46b1c287adba728258d0aa28f8
mathhtam15 <102478488+mathhtam15@users.noreply.github.com>
2022-05-23 21:41:29 +0200

    Add files via upload

---
+++ data-raw/language.R
@@ -0,0 +1,5 @@
+## code to prepare `language` dataset goes here
+
+language <- data.frame(code = c("en", "fr", "it", "es", "de"), hello = c("Hello", "Bonjour", "Bonjourno", "Hola", "Hallo"))
+
+usethis::use_data(language, overwrite = TRUE)
